18546214f00d17dde86921e5deb1a0ccc908e23c
ATXLtheAxolotl <flashtube107@gmail.com>
2022-07-23 21:36:53 -0400

    Add(): Riptide Network lib

---
+++ Assets/Scripts/NewJoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewJoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Connect() {
+        RakClient.Connect("127.0.0.1", 8000);
+        RakClient.OnInitialized += () => {
+            Debug.Log("Initialized");
+        };
+    }
+}
